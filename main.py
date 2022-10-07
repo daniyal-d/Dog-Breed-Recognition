868e8fe417c3e3efb26ec9295de4780c33d6a82a
Daniyal <31736868+daniyal-d@users.noreply.github.com>
2022-10-06 19:02:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ from tensorflow.keras.preprocessing.image import save_img
 from PIL import Image
 
 with st.sidebar:
-    st.markdown("""This project was trained on Kaggle's Dog Breed Identification dataset
-    https://www.kaggle.com/competitions/dog-breed-identification/data""")
+    st.markdown("""This project was trained on the Stanford ImageNet Dog Breed dataset
+    http://vision.stanford.edu/aditya86/ImageNetDogs/menu_frame.html""")
     st.markdown("Due to this, the model can only properly predict a dog breed if it was trained on it. Please note "
-                "that the Kaggle dataset does not contain certain dog breeds (such as Shiba Inu), and therefore the "
+                "that the dataset does not contain certain dog breeds (such as Shiba Inu), and therefore the "
                 "model may not be able to properly predict one.")
     st.markdown("This model still scored over 80% on the validation data, however, note that the validation data came "
-                "from the Kaggle dataset") 
+                "from the ImageNet dataset") 
 
 
 st.title("Dog Breed Recognition")
 st.subheader("This project uses Deep Learning and MobileNetV2 to detect the breeds of dogs from images. It was "
-             "trained using Kaggle's Dog Breed Identification dataset.")
+             "trained using the Stanford Dog Breed dataset.")
 st.subheader("The model has 81.12% accuracy on the validation data and 99.9% accuracy on the train data.")
 st.subheader("Upload a photo below to get the model's predictions")
 st.caption("Created by Daniyal Dawood")
